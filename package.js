c34c7502ad17fa8363985784d7ceaffd5df44085
Jonathan Bastnagel <jbastnagel@mentortek.com>
2016-02-16 12:10:47 -0600

    Specify package version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'jbastnagelmt:app-notifications',
   version: '0.0.2',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Meteor In-App Notifications',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/inkadnb/meteor-app-notifications',
   // By default, Meteor will default to using README.md for documentation.
@@ -20,7 +20,7 @@ Package.onUse(function(api) {
   api.use('reactive-var');
   api.use('blaze-html-templates');
   api.use('mongo');
-  api.use("kadira:blaze-layout");
+  api.use("kadira:blaze-layout@2.3.0");
   api.addFiles('app-notifications-shared.js');
   api.addFiles('app-notifications-server.js','server');
   api.addFiles('app-notifications.html','client');
